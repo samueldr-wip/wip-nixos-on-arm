c551b20243778a53e2b2cdab2eb97b4f6c06629d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-22 20:10:35 -0400

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,54 @@
+{ pkgs ? import ./nixpkgs.nix {} }:
+
+let
+  nixpkgsPath = pkgs.path;
+  fromPkgs = path: nixpkgsPath + "/${path}";
+  evalConfig = import (fromPkgs "nixos/lib/eval-config.nix");
+  buildConfig = { configuration ? {} }:
+    evalConfig {
+      specialArgs = {
+        inherit nixpkgsPath;
+      };
+      modules= [
+          ./modules
+          configuration
+      ];
+    }
+  ;
+  devices =
+    builtins.filter
+    (d: builtins.pathExists (./. + "/devices/${d}/default.nix"))
+    (builtins.attrNames (builtins.readDir ./devices))
+  ;
+
+  evalDevice =
+    name: 
+    let
+      evalWith = additionalConfig: buildConfig {
+        configuration =
+          {
+            imports = [
+              additionalConfig
+              (./devices + "/${name}")
+            ];
+          }
+        ;
+      };
+    in
+    rec {
+      isoMinimal = (
+        evalWith (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+      ).config.system.build.isoImage;
+
+      rootfs = (
+        evalWith ./modules/rootfs.nix
+      ).config.system.build.rootfsImage;
+
+      eval = evalWith { };
+
+      inherit (eval) pkgs;
+    }
+  ;
+in
+  builtins.listToAttrs
+  (map (name: { inherit name; value = evalDevice name; }) devices)
